d6dc2c1d4de934421d8a9221be6d642bcd83399f
Imran Matin <imranmatin23@gmail.com>
2024-04-29 08:00:41 -0700

    Initial Commit

---
+++ myfirstiosapp/myfirstiosappApp.swift
@@ -0,0 +1,17 @@
+//
+//  myfirstiosappApp.swift
+//  myfirstiosapp
+//
+//  Created by Imran Matin on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct myfirstiosappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
